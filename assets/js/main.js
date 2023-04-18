83f894de8305c004ce4c4c19dbfd2ad3f717355f
OksanaKarpyn <93349703+OksanaKarpyn@users.noreply.github.com>
2023-04-18 13:15:42 +0200

    add foto

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,9 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        messagge:'Hello Vue'
+        messagge:'Hello Vue',
+        imageSrc:'https://picsum.photos/seed/picsum/200/300',
+        imageName:'paesaggio',
       }
     }
   }).mount('#app')
